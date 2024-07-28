2ef5b81be1377f710cfd54f86547e13420a0cac1
Tayebe kavousi <tayebekavousi68@gmail.com>
2024-07-29 04:00:22 +0800

    debug

--- frontend/app.js
+++ frontend/app.js
@@ -5,24 +5,36 @@ document.getElementById('greet-form').addEventListener('submit', async function(
     // const url = 'http://localhost:5000/greet';
     const url = 'http://backend-service.magicmusic.local:5000/greet';
     // const url = 'http://backend:5000/greet';
-    const data = {"name": name};
+    // const data = {"name": name};
 
-    const headers={
-        'Content-type':'application/json', 
-        'Accept':'application/json'
-    }
+    // const headers={
+    //     'Content-type':'application/json', 
+    //     'Accept':'application/json'
+    // }
 
-    try {
-        const response = await fetch(url, {
-            method: 'POST', 
-            headers: headers,
-            body: JSON.stringify(data),
-        });
-        const result = await response.json();
-        console.log(result);
-        document.getElementById('greeting-message').innerText = result.message;
-    } catch (error) {
-        console.error('Error:', error);
-    }
+    // try {
+    //     const response = await fetch(url, {
+    //         method: 'POST', 
+    //         headers: headers,
+    //         body: JSON.stringify(data),
+    //     });
+    //     const result = await response.json();
+    //     console.log(result);
+    //     document.getElementById('greeting-message').innerText = result.message;
+    // } catch (error) {
+    //     console.error('Error:', error);
+    // }
+
+    fetch('http://backend-service.magicmusic.local:5000/greet', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ message: 'Hello from frontend!' })
+      })
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(error => console.error('Error:', error));
+      
 });
 
