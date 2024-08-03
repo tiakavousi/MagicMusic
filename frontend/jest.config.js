c6acad6c3e2915cb7b18d8f7e9c9ff944fa98d17
Tayebe kavousi <tayebekavousi68@gmail.com>
2024-08-03 20:05:09 +0800

    jest config added

---
+++ frontend/jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  testMatch: [
+    "**/src/__tests__/**/*.js?(x)",
+  ],
+  setupFiles: ['<rootDir>/jest.setup.js'],
+  transform: {
+    "^.+\\.jsx?$": "babel-jest"
+  },
+  transformIgnorePatterns: [
+    "/node_modules/(?!@testing-library/)"
+  ],
+  testEnvironment: 'jest-environment-jsdom',
+};
