376f572355b2d58932086b49bdca7a9b965e78a8
Tayebe kavousi <tayebekavousi68@gmail.com>
2024-08-16 00:26:24 +0800

    change the url

--- app/index.js
+++ app/index.js
@@ -1,41 +1,39 @@
-let color = process.env.COLOR_BACKGROUND || 'white';
+const http = require('http');
+const url = require('url');
+
 const port = process.env.COLOR_PORT || 8080;
 const fqdn = process.env.HOSTNAME || 'localhost';
-const http = require('http');
 
-http.createServer(function (request, response) {
+http.createServer((req, res) => {
+  const parsedUrl = url.parse(req.url, true);
+  const pathname = parsedUrl.pathname;
 
-    if (request.url === '/red') {
-        color = "red"
-        console.log('Change color to:', color);
-    }
-    if (request.url === '/white') {
-        color = "white"
-        console.log('Change color to:', color);
-    }
-    if (request.url === '/green') {
-        color = "green"
-        console.log('Change color to:', color);
-    }
+  // Regular expression to match the URL pattern
+  const colorRegex = /\/(prod|qa)\/(\w+)/;
+  const match = pathname.match(colorRegex);
 
-    response.writeHead(200, { 'Content-Type': 'text/html' });
-    response.write('<html>' +
-        '    <head>' +
-        '        <title>color ' + color + '</title>' +
-        '        <style type="text/css">' +
-        '            body {background-color:  ' + color + '}' +
-        '        </style>' +
-        '    </head>' +
-        '    <body>' +
-        '        <h1>' + color + '</h1>' +
-        '        <h2>Running on ' + fqdn + '</h2>' +
-        '        <h2><a href="red">red</a></h2>' +
-        '        <h2><a href="white">white</a></h2>' +
-        '        <h2><a href="green">green</a></h2>' +
-        '    <body>' +
-        '</html>');
-    response.end();
+  let color = 'white'; // Default color
+  if (match) {
+    color = match[2]; // Extract the color from the match
+  }
 
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<html>' +
+    '    <head>' +
+    '        <title>color ' + color + '</title>' +
+    '        <style type="text/css">' +
+    '            body {background-color:  ' + color + '}' +
+    '        </style>' +
+    '    </head>' +
+    '    <body>' +
+    '        <h1>' + color + '</h1>' +
+    '        <h2>Running on ' + fqdn + '</h2>' +
+    '        <h2><a href="/prod/red">red</a></h2>' +
+    '        <h2><a href="/prod/green">green</a></h2>' +
+    '        <h2><a href="/prod/white">white</a></h2>' +
+    '    <body>' +
+    '</html>');
+  res.end();
 }).listen(parseInt(port, 10));
 
 console.log("HTTP server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
